3f19839b0b76f4ad7f714908826f2a74b8a6a8c3
RFlintstone <rubenflint@hotmail.com>
2024-11-03 17:54:16 +0100

    Removed decryption methods

--- api/Services/Auth/EncryptionService.cs
+++ api/Services/Auth/EncryptionService.cs
@@ -18,12 +18,6 @@ public class EncryptionService
     {
         return Encrypt(plaintext, encryptionKey, iv);
     }
-
-    // public string Decrypt(byte[] ciphertext)
-    // {
-    //     return Decrypt(ciphertext, encryptionKey, iv);
-    // }
-    //
     
     public static byte[] Encrypt(string plaintext, byte[] key, byte[] iv)
     {
@@ -47,28 +41,4 @@ public class EncryptionService
             return encryptedBytes;
         }
     }
-
-    // public static string Decrypt(byte[] ciphertext, byte[] key, byte[] iv)
-    // {
-    //     using (Aes aesAlg = Aes.Create())
-    //     {
-    //         aesAlg.Key = key;
-    //         aesAlg.IV = iv;
-    //         ICryptoTransform decryptor = aesAlg.CreateDecryptor(aesAlg.Key, aesAlg.IV);
-    //         byte[] decryptedBytes;
-    //         using (var msDecrypt = new System.IO.MemoryStream(ciphertext))
-    //         {
-    //             using (var csDecrypt = new CryptoStream(msDecrypt, decryptor, CryptoStreamMode.Read))
-    //             {
-    //                 using (var msPlain = new System.IO.MemoryStream())
-    //                 {
-    //                     csDecrypt.CopyTo(msPlain);
-    //                     decryptedBytes = msPlain.ToArray();
-    //                 }
-    //             }
-    //         }
-    //
-    //         return Encoding.UTF8.GetString(decryptedBytes);
-    //     }
-    // }
 }
