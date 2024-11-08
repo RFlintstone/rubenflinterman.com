a1b85adcff0bdcbb28701c827660d6d9395d80e1
RFlintstone <rubenflint@hotmail.com>
2024-11-08 16:41:21 +0100

    Updated Dockerfile

---
+++ api-old/target/classes/db/migration/V1__Initial_setup.sql
@@ -0,0 +1,15 @@
+-- V1__Initial_setup.sql
+-- This is a SQL script that will be executed when the application starts
+
+-- Creating the users table, if it doesn't exist
+CREATE TABLE IF NOT EXISTS users (
+    id SERIAL PRIMARY KEY,
+    username VARCHAR(50) NOT NULL,
+    password VARCHAR(100) NOT NULL,
+    roles VARCHAR(50) NOT NULL
+    token VARCHAR(100) NOT NULL
+);
+
+-- Inserting some data
+INSERT INTO users (username, password, roles) VALUES ('admin', 'adminpassword', 'ROLE_ADMIN', 'token');
+INSERT INTO users (username, password, roles) VALUES ('userModel', 'userpassword', 'ROLE_USER', 'token');
