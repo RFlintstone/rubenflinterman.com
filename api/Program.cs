20f623f11639b468c1ad419047c07193610617cf
rflintstone <rubenflint@hotmail.com>
2024-11-10 16:52:11 +0100

    Workflow with repo secret

--- api/Program.cs
+++ api/Program.cs
@@ -52,8 +52,17 @@ class Program
         // Load the X.509 certificate
         if (!isInDocker)
         {
-            var certificatePath = Path.Combine(Directory.GetCurrentDirectory(), "certificate.pfx");
-            var certificate = new X509Certificate2(certificatePath, configuration["Certificate:Key"]);
+            var certificatePath;
+            var certificate;
+            try
+            {
+                certificatePath = Path.Combine(Directory.GetCurrentDirectory(), "certificate.pfx");
+                certificate = new X509Certificate2(certificatePath, configuration["Certificate:Key"]);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error loading certificate: {ex.Message}");
+            }
             builder.Services.AddDataProtection()
                 .PersistKeysToFileSystem(new DirectoryInfo(@"/keys/"))
                 .ProtectKeysWithCertificate(certificate);
