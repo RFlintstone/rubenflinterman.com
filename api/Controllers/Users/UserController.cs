67a3be0b0e4408e90eda0fb66b817134afee0eb1
RFlintstone <rubenflint@hotmail.com>
2024-11-13 21:25:42 +0100

    Add user admin and update user info model with roles
    - Added an admin user with ID "Admin" and default password "admin".
    - Updated the UserInfoModel class to include a Roles property with a default value of ["User"].
    - Updated the database context seed method to create users with unique IDs, usernames, emails, and passwords.

--- api/Controllers/Users/UserController.cs
+++ api/Controllers/Users/UserController.cs
@@ -13,6 +13,21 @@ public class UserController(UserInfoService userInfoService) : ControllerBase
     {
         userInfoService.SetUsername(User);
         userInfoService.SetId(User);
-        return Ok($"You are logged in as {userInfoService.GetUsername()} with id: {userInfoService.GetId()}");
+        userInfoService.SetRoles(User);
+        return Ok($"You are logged in as {userInfoService.GetUsername()} with id: {userInfoService.GetId()} and roles: {string.Join(", ", userInfoService.GetRoles())}");
+    }
+
+    [HttpGet("admin")]
+    [Authorize]
+    public IActionResult GetAdmin()
+    {
+        userInfoService.SetRoles(User);
+        if (userInfoService.GetRoles().Contains("Admin"))
+        {
+            userInfoService.SetUsername(User);
+            userInfoService.SetId(User);
+            return Ok($"You are logged in as administrator");
+        }
+        return Ok($"You are not an administrator");
     }
 }
