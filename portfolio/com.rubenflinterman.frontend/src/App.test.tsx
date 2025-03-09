608008396af813fe61984886d3087ae13d7e4b6a
Ruben Flinterman <rubenflint@hotmail.com>
2025-03-09 13:00:38 +0100

    Update App.test.tsx

--- portfolio/com.rubenflinterman.frontend/src/App.test.tsx
+++ portfolio/com.rubenflinterman.frontend/src/App.test.tsx
@@ -3,6 +3,6 @@ import Home from "./views/Home";
 
 test('renders learn react link', () => {
     render(<Home/>);
-    const linkElement = screen.getByText(/ProjectsIncluded/i);
+    const linkElement = screen.getByText(/Software engineer by day, creative by night./i);
     expect(linkElement).toBeInTheDocument();
 });
